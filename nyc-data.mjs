ed5b2c75b42a7a28c44cfd320505aa812b4a6cb9
Myles Borins <myles.borins@gmail.com>
2020-04-02 03:03:04 -0400

    more info

--- nyc-data.mjs
+++ nyc-data.mjs
@@ -7,15 +7,24 @@ async function main() {
   const parser = readStream.pipe(parse());
   let tested = 0;
   let positive = 0;
+  let mostTested = 0;
+  let mostTestedDate = '';
+  let avgTested = 0;
   let count = 0;
   for await (const record of parser) {
     if (count !== 0) {
-      tested += Number(record[2]);
+      let numtested = Number(record[2]);
+      tested += numtested;
       positive += Number(record[3]);
+      if (numtested > mostTested) {
+        mostTested = numtested;
+        mostTestedDate = record[1];
+      }
     }
     count++;
   }
-  console.log(`\nPositive: ${positive}\nTested: ${tested}\n`);
+  avgTested = tested / (count - 1);
+  console.log(`\nPositive: ${positive}\nTested: ${tested}\n\nMost Tested: ${mostTested} on ${mostTestedDate}\nAverage Tested: ${avgTested}\n`);
 }
 
 main().catch(e => console.error(e));
