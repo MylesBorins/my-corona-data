c9007edfca8627cf49749af434cc2004a2e0d7b5
Myles Borins <myles.borins@gmail.com>
2020-04-13 01:15:23 -0400

    Squashed 'third_party/coronavirus-data/' changes from 3fdd59a..d34e6aa
    d34e6aa Updated with data as of 4/12
    8542fbf Updated with 4/11 data
    
    git-subtree-dir: third_party/coronavirus-data
    git-subtree-split: d34e6aab1e0dd0e0125e74519489e7893d33c9dd

--- index.mjs
+++
@@ -1,7 +0,0 @@
-async function main() {
-  console.log('NYC Corona Data\n');
-  // await import('./testing.mjs');
-  await import('./new-york-city-data.mjs');
-}
-
-main().catch(e => console.error(e));
