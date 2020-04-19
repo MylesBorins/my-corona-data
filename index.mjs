76937752c955814b52dd399b2716b930c1119321
Myles Borins <myles.borins@gmail.com>
2020-04-18 21:45:13 -0400

    the lack of space gave me anxiety

--- index.mjs
+++ index.mjs
@@ -13,6 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+
 async function main() {
   await import('./new-york-state-data.mjs');
   await import('./new-york-city-data.mjs');
