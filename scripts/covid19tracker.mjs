adeb2e0211f2f324aee8959b54c837b53a89e885
Myles Borins <myles.borins@gmail.com>
2021-01-15 22:04:44 -0500

    tools: maybe get rid of some update scripts

--- scripts/covid19tracker.mjs
+++ scripts/covid19tracker.mjs
@@ -1,13 +1,14 @@
 import fetch from 'node-fetch';
 import { writeFile } from 'fs/promises';
 
-const route = 'reports/province/on';
-const baseUrl = `https://api.covid19tracker.ca/${route}/`;
-const query = '';
+const baseUrl = `https://api.covid19tracker.ca/`;
+const ontarioRoute = 'reports/province/on/';
+const query = '?';
   // 'county==%27Richmond%27';
 
-const outputPath = '../third_party/api.covid19tracker.ca/covidtracker.ca-ontario-data.json';
+const outputPathOntario = '../third_party/api.covid19tracker.ca/covidtracker.ca-ontario-data.json';
+const outputPathToronto = '../third_party/api.covid19tracker.ca/covidtracker.ca-toronto-data.json'
 
-const req = await fetch(`${baseUrl}?${query}`);
+const req = await fetch(`${baseUrl}${ontarioRoute}${query}`);
 const json = await req.json();
-await writeFile(new URL(outputPath, import.meta.url), JSON.stringify(json, undefined, 1));
+await writeFile(new URL(outputPathOntario, import.meta.url), JSON.stringify(json, undefined, 1));
