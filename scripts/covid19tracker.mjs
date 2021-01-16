5f64523cb82c338e2147d0814cd2691c9f4c51ea
Myles Borins <myles.borins@gmail.com>
2021-01-15 21:54:59 -0500

    data: change name of ontario data json

--- scripts/covid19tracker.mjs
+++ scripts/covid19tracker.mjs
@@ -6,7 +6,7 @@ const baseUrl = `https://api.covid19tracker.ca/${route}/`;
 const query = '';
   // 'county==%27Richmond%27';
 
-const outputPath = '../third_party/api.covid19tracker.ca/covidtracker.ca-data.json';
+const outputPath = '../third_party/api.covid19tracker.ca/covidtracker.ca-ontario-data.json';
 
 const req = await fetch(`${baseUrl}?${query}`);
 const json = await req.json();
